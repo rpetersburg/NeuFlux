7678529df34a00d89cee6c5df332dd10a75a0a87
Kevin Wierman <kwierman@emailunc.edu>
2013-09-29 21:01:53 -0400

    Building Documentation

--- include/NeuFlux.hh
+++ include/NeuFlux.hh
@@ -1,4 +1,36 @@
-/*
+/*!
 	\mainpage Neuflux
 	\brief  The low depth Neutron flux Simulation
+
+	\section intro Introduction
+
+	This is the placeholder simulation for Neutron Flux at low depths (Neuflux).
+
+	---	
+
+	\section dep Dependencies
+
+	The following libraries must be built and in the paths in order to build properly:
+
+	-  ROOT
+		-  <a href="http://root.cern.ch/drupal/">ROOT Website</a> 
+		- Required for file input and output
+	-  Geant4
+		- <a href="http://geant4.cern.ch/">Geant4 Website</a>
+		- Required for the simulation libraries
+	-  CMake
+		- <a href="http://www.cmake.org/">CMake Website</a>
+		- Required for building the program
+
+	---
+
+	\section building Building
+
+	In order to Build 
+	
+	---
+	\section running Running
+
+	In order to run
+	---
 */

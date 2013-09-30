c8997af7db492fcd4394d2d46993ac922bcf461a
Kevin Wierman <kwierman@emailunc.edu>
2013-09-29 21:20:37 -0400

    Still Working on Documentation

--- include/NeuFlux.hh
+++ include/NeuFlux.hh
@@ -1,12 +1,40 @@
 /*!
 	\mainpage Neuflux
-	\brief  The low depth Neutron flux Simulation
+
+	\tableofcontents
 
 	\section intro Introduction
 
-	This is the placeholder simulation for Neutron Flux at low depths (Neuflux).
+	This is the placeholder simulation for Neutron Flux at low depths (NeutronFlux, or NeuFlux for short).
+	The prupose of this software is to 
 
-	---	
+	Copyright © 2013 University of North Carolina at Chapel Hill. All rights reserved.
+
+	---
+
+	This software was prepared for the Regents of the University of <br>
+	North Carolina at the Deptment of Physics and Astronomy sponsored <br>
+	in part by the United States Department of Energy (DOE) under <br>
+	Grant #DE-FG02-97ER41020. The University has certain rights in the <br> 
+	program pursuant to the contract and the program should not be  <br>
+	copied or distributed outside your organization.  The DOE and the  <br>
+	University of North Carolina reserve all rights in the program.  <br>
+	Neither the authors, University of North Carolina, or U.S. Government <br> 
+	make any warranty, express or implied, or assume any liability or <br>
+	responsibility for the use of this software.
+	
+	---
+	
+	\section downloading Downloading Software
+
+	This software is hosted on <a href="https://github.com/">GitHub</a>. 
+	The URL for the repository is: https://github.com/kwierman/NeuFlux .
+
+	Pulling from the repository can be accomplished by downloading <a href="https://github.com/">Git</a>
+
+		git clone https://github.com/kwierman/NeuFlux.git
+	
+	---
 
 	\section dep Dependencies
 
@@ -22,11 +50,12 @@
 		- <a href="http://www.cmake.org/">CMake Website</a>
 		- Required for building the program
 
+
 	---
 
 	\section building Building
 
-	In order to Build 
+	In order to Build the program
 	
 	---
 	\section running Running
